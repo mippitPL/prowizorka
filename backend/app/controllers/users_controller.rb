86518db85c9adbe5c18f33dd9f10c5f6d17dc6e4
Rafał Łasocha <me+dev@swistak35.com>
2016-04-15 20:08:42 +0200

    fixed indentation

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,25 +1,25 @@
 class UsersController < ApplicationController
   def show
-	user = User.where(device_id: params[:id]).first
-	render json: user
+    user = User.where(device_id: params[:id]).first
+    render json: user
   end
 
   def create
-	user = User.create!(
-	  name:        params[:name],
-	  description: params[:description],
-	  device_id:   params[:device_id])
+    user = User.create!(
+      name:        params[:name],
+      description: params[:description],
+      device_id:   params[:device_id])
 
-	render json: user
+    render json: user
   end
 
   def update
-	user = User.where(device_id: params[:id]).first
+    user = User.where(device_id: params[:id]).first
 
-	user.update_attributes(
-	  name:        params[:name],
-	  description: params[:description])
+    user.update_attributes(
+      name:        params[:name],
+      description: params[:description])
 
-	render json: user
+    render json: user
   end
 end
--- backend/test/controllers/users_controller_test.rb
+++ backend/test/controllers/users_controller_test.rb
@@ -2,48 +2,48 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   test "should get show" do
-	User.create!(
-	  name: "Kuba Kowalski",
-	  description: "jakis noob",
-	  device_id: "abcd")
+    User.create!(
+      name: "Kuba Kowalski",
+      description: "jakis noob",
+      device_id: "abcd")
 
-	get :show, id: "abcd"
+    get :show, id: "abcd"
 
-	assert_response :success
+    assert_response :success
 
-	body = JSON.parse(response.body)
+    body = JSON.parse(response.body)
 
-	assert_equal(body["name"],        "Kuba Kowalski")
-	assert_equal(body["description"], "jakis noob")
-	assert_equal(body["device_id"],   "abcd")
+    assert_equal(body["name"],        "Kuba Kowalski")
+    assert_equal(body["description"], "jakis noob")
+    assert_equal(body["device_id"],   "abcd")
   end
 
   test "lol" do
-	post :create,
-	  name: "Kuba Kowalski",
-	  description: "jakis noob",
-	  device_id: "abcd"
+    post :create,
+      name: "Kuba Kowalski",
+      description: "jakis noob",
+      device_id: "abcd"
 
-	body = JSON.parse(response.body)
+    body = JSON.parse(response.body)
 
-	assert_equal(body["name"],        "Kuba Kowalski")
-	assert_equal(body["description"], "jakis noob")
-	assert_equal(body["device_id"],   "abcd")
+    assert_equal(body["name"],        "Kuba Kowalski")
+    assert_equal(body["description"], "jakis noob")
+    assert_equal(body["device_id"],   "abcd")
   end
 
   test "update" do
-	post :create,
-	  name: "Kuba Kowalski",
-	  description: "jakis noob",
-	  device_id: "abcd"
+    post :create,
+      name: "Kuba Kowalski",
+      description: "jakis noob",
+      device_id: "abcd"
 
-	put :update,
-	  id:          "abcd",
-	  name:        "Jan Kowalski",
-	  description: "moze jednak nie noob"
+    put :update,
+      id:          "abcd",
+      name:        "Jan Kowalski",
+      description: "moze jednak nie noob"
 
-	u = User.last
-	assert_equal(u.name,        "Jan Kowalski")
-	assert_equal(u.description, "moze jednak nie noob")
+    u = User.last
+    assert_equal(u.name,        "Jan Kowalski")
+    assert_equal(u.description, "moze jednak nie noob")
   end
 end
