e7cf6854bbd76519918566813f262379d2e0f73c
Rafał Łasocha <me+dev@swistak35.com>
2016-04-16 00:58:35 +0200

    backend

--- backend/app/models/performance.rb
+++ backend/app/models/performance.rb
@@ -1,3 +1,24 @@
 class Performance < ActiveRecord::Base
   belongs_to :user
+
+  def likes
+	42
+  end
+
+  def picture_url
+	arr = [
+	  "http://192.168.47.42:3000/pic1.jpg",
+	  "http://192.168.47.42:3000/pic1.jpg",
+	  "http://192.168.47.42:3000/pic1.jpg",
+	  "http://192.168.47.42:3000/pic1.jpg"
+	]
+	arr[user_id % 4]
+  end
+
+  def as_json(*)
+	super.merge({
+	  likes:       likes,
+	  picture_url: picture_url,
+	})
+  end
 end
