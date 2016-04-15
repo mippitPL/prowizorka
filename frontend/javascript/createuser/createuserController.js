7ae0b63a31a68777b3ebaa5325b834d7fe7efaac
Rafał Łasocha <me+dev@swistak35.com>
2016-04-15 22:39:58 +0200

    createuser form

--- frontend/javascript/createuser/createuserController.js
+++ frontend/javascript/createuser/createuserController.js
@@ -3,5 +3,23 @@ angular.module('taxApp').controller('createuserController', createuserController
 angular.$inject = ['$scope', 'geolocation']
 
 function createuserController($scope) {
-	$scope.hello = "Hello World!";
+	$scope.newUser = {
+		name:        '',
+		description: ''
+	}
+
+	function resetCreateUserForm() {
+		$scope.newUser = {
+			name:        '',
+			description: ''
+		}
+	}
+
+	function submitCreateUser(user) {
+		console.warn(user);
+
+		// resetCreateUserForm();
+	}
+
+	$scope.submitCreateUser = submitCreateUser;
 }
