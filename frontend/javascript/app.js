1b6a1bb491fe4ec92fec18911f5ad652c6bdd61f
Jakub Kowalski <voodoo.mh@gmail.com>
2016-04-15 20:19:28 +0200

    init mobile angular ui

--- frontend/javascript/app.js
+++ frontend/javascript/app.js
@@ -1,10 +1,14 @@
-var app = angular.module('taxApp', ['ngRoute']);
+angular
+	.module('taxApp', [
+		'ngRoute',
+		'mobile-angular-ui'
+	])
 
-app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
- $routeProvider
-      .when('/start', {
-        templateUrl: 'javascript/start/startView.html', 
-        controller: 'startController'
-      })
-}]); 
- 
+	.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+		$routeProvider
+	     	.when('/start', {
+	        	templateUrl: 'javascript/start/startView.html', 
+	        	controller: 'startController'
+	      	})
+	}]); 
+	 
