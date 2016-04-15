bedb681cd3fd71b42a777a6fad377506e42fe4eb
Kamil Banach <kontakt@elwin013.com>
2016-04-15 21:07:27 +0200

    geoloc libs

--- frontend/javascript/lib/geoloc.js
+++ frontend/javascript/lib/geoloc.js
@@ -2,3 +2,18 @@
  * Created by elwin013 on 15.04.16.
  * File for geolocalization methods.
  */
+var currentPosition = {lat: "", lng: "", err: ""};
+function getCurrentLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(pst) {
+            currentPosition.lat = pst.coords.latitude;
+            currentPosition.lng = pst.coords.longitude;
+            currentPosition.err = "";
+        }, function() {
+            currentPosition = { lat: "", lng: "", err: "Unknown error"};
+        });
+    } else {
+        currentPosition = {lat: "", lng: "", err: "Don't support localization"};
+    }
+    return currentPosition;
+}
