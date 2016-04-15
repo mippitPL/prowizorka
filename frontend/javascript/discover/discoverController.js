0264ad6f311298278177f8a9544626fcbce79bca
Jakub Kowalski <voodoo.mh@gmail.com>
2016-04-16 00:06:56 +0200

    sidebar

--- frontend/javascript/discover/discoverController.js
+++ frontend/javascript/discover/discoverController.js
@@ -18,6 +18,6 @@ function discoverController($scope, geolocation, $rootScope, httpService) {
     });
 
     $scope.showPerfModal = function (perf) {
-    	alert(perf.id);
+    	alert(perf.lat);
     }
 }
