59736fe917e55cb28a7be0f1270d7cb81a35a7d3
Jakub Kowalski <voodoo.mh@gmail.com>
2016-04-16 04:44:45 +0200

    merge start with create user

--- frontend/javascript/start/startController.js
+++ frontend/javascript/start/startController.js
@@ -2,11 +2,48 @@ angular
 	.module('taxApp')
 	.controller('startController', startController);
 
-startController.$inject = ['$scope', '$location'];
+startController.$inject = ['$scope', '$location', 'httpService'];
 
-function startController($scope, $location) {
-	$scope.chuj = "dupa";
-	if (localStorage.getItem("userId")) {
+function startController($scope, $location, httpService) {
+	if (localStorage.getItem("userId") != null) {
+		console.log(localStorage.getItem("userId"));
 		$location.url("/discover");
 	}
+
+	$scope.newUserId = {}
+	$scope.newUser = {
+		name:        '',
+		description: ''
+	}
+
+
+	function resetCreateUserForm() {
+		$scope.newUser = {
+			name:        '',
+			description: ''
+		}
+	}
+
+	function submitCreateUser(user) {
+		console.warn(user);
+		httpService.createUser(user).then(function(data) {
+			$scope.newUserId = data.data.id;
+			localStorage.setItem("userId", $scope.newUserId);
+			$location.path("addperformance");
+		});
+		// resetCreateUserForm();
+	}
+
+	function humanSubmit(user) {
+		console.warn(user);
+		httpService.createUser(user).then(function(data) {
+			$scope.newUserId = data.data.id;
+			localStorage.setItem("userId", $scope.newUserId);
+			$location.path("discover");
+		});
+		// resetCreateUserForm();
+	}
+
+	$scope.submitCreateUser = submitCreateUser;
+	$scope.humanSubmit = humanSubmit;
 }
